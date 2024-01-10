9b3a6cfc7f5e3ccbc0531105db743ba2c8ee86a2
Robert Zubek <robert.zubek.ofc@gmail.com>
2024-01-10 20:15:22 +0100

    DHT11 connected

--- Kasztan/Kasztan.ino
+++ Kasztan/Kasztan.ino
@@ -1,9 +1,23 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <DHT.h>
 
+#define dhtPIN 2 //D4
+
+#define dhtTYPE DHT11
+
+DHT dht(dhtPIN, dhtTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(1000);
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
 
+  Serial.println("Temperature:");
+  Serial.println(temperature);
+  Serial.println("Humidity:");
+  Serial.println(humidity);
 }
