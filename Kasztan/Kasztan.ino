a46eb2ca2e97d755aa2e39b8c5f0af42f7af3864
Robert Zubek <robert.zubek.ofc@gmail.com>
2024-01-11 10:39:14 +0100

    sending data via html and rgb led control

--- Kasztan/Kasztan.ino
+++ Kasztan/Kasztan.ino
@@ -4,6 +4,9 @@
 
 #define dhtPIN 2 //D4
 #define fotoPIN A0
+#define redPIN 0//D3
+#define greenPIN 4//D2
+#define bluePIN 5//D1
 
 #define dhtTYPE DHT11
 
@@ -14,16 +17,34 @@ float temperature;
 float humidity;
 float light;
 
+int redValue = 128;
+int greenValue = 128;
+int blueValue = 128;
+
 DHT dht(dhtPIN, dhtTYPE);
 WiFiClient client;
 ESP8266WebServer server(80);   
 
 void setup() {
 
+  pinMode(redPIN, OUTPUT);
+  pinMode(greenPIN, OUTPUT);
+  pinMode(bluePIN, OUTPUT);
+/*
+  analogWrite(redPIN, 128);
+  analogWrite(bluePIN, 128);
+  analogWrite(greenPIN, 128);*/
+
   Serial.begin(9600);
   dht.begin();
   connectToWiFi();
   server.on("/", handle_OnConnect);
+  server.on("/increaseRed", HTTP_GET, handle_IncreaseRed);
+  server.on("/decreaseRed", HTTP_GET, handle_DecreaseRed);
+  server.on("/increaseGreen", HTTP_GET, handle_IncreaseGreen);
+  server.on("/decreaseGreen", HTTP_GET, handle_DecreaseGreen);
+  server.on("/increaseBlue", HTTP_GET, handle_IncreaseBlue);
+  server.on("/decreaseBlue", HTTP_GET, handle_DecreaseBlue);
   server.onNotFound(handle_NotFound);
 
   server.begin();
@@ -72,34 +93,93 @@ void handle_OnConnect() {
   temperature = dht.readTemperature();
   humidity = dht.readHumidity();
   light = analogRead(fotoPIN);
-  server.send(200, "text/html", SendHTML(temperature,humidity, light)); 
+  server.send(200, "text/html", SendHTML(temperature,humidity, light, redValue, greenValue, blueValue)); 
+}
+
+void handle_IncreaseRed()
+{
+  if (redValue < 255)
+    redValue-=10;
+  analogWrite(redPIN, redValue);
+  server.send(200, "text/html", SendHTML(temperature, humidity, light, redValue, greenValue, blueValue));
+}
+void handle_IncreaseGreen()
+{
+  if (greenValue < 255)
+    greenValue-=10;
+  analogWrite(greenPIN, greenValue);
+  server.send(200, "text/html", SendHTML(temperature, humidity, light, redValue, greenValue, blueValue));
+}
+void handle_IncreaseBlue()
+{
+  if (blueValue < 255)
+    blueValue-=10;
+  analogWrite(bluePIN, blueValue);
+  server.send(200, "text/html", SendHTML(temperature, humidity, light, redValue, greenValue, blueValue));
+}
+void handle_DecreaseRed()
+{
+  if (redValue > 0)
+    redValue+=10;
+  analogWrite(redPIN, redValue);
+  server.send(200, "text/html", SendHTML(temperature, humidity, light, redValue, greenValue, blueValue));
+}
+void handle_DecreaseGreen()
+{
+  if (greenValue > 0)
+    greenValue+=10;
+  analogWrite(greenPIN, greenValue);
+  server.send(200, "text/html", SendHTML(temperature, humidity, light, redValue, greenValue, blueValue));
+}
+void handle_DecreaseBlue()
+{
+  if (blueValue > 0)
+    blueValue+=10;
+  analogWrite(bluePIN, blueValue);
+  server.send(200, "text/html", SendHTML(temperature, humidity, light, redValue, greenValue, blueValue));
 }
 
 void handle_NotFound(){
   server.send(404, "text/plain", "Not found");
 }
 
-String SendHTML(float temperature,float humidity, float light){
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<title>ESP8266 Weather Station</title>\n";
-  ptr +="<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
-  ptr +="p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
-  ptr +="</style>\n";
-  ptr +="</head>\n";
-  ptr +="<body>\n";
-  ptr +="<div id=\"webpage\">\n";
-  ptr +="<h1>KASZTAN Weather Station</h1>\n";
-  ptr +="<p>Temperature: ";
-  ptr +=temperature;
-  ptr +="&deg;C</p>";
-  ptr +="<p>Humidity: ";
-  ptr +=humidity;
-  ptr +="%</p>";
-  ptr +="<p>Light: ";
-  ptr +=light;
-  ptr+="<p>by Robert Zubek";
+String SendHTML(float temperature,float humidity, float light, int red, int green, int blue){
+   String ptr = "<!DOCTYPE html> <html>\n";
+  ptr += "<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
+  ptr += "<title>ESP8266 Weather Station</title>\n";
+  ptr += "<script>function increaseColor(color) {"
+         "var xhr = new XMLHttpRequest();"
+         "xhr.open('GET', '/increase' + color, true);"
+         "xhr.send();}"
+         "function decreaseColor(color) {"
+         "var xhr = new XMLHttpRequest();"
+         "xhr.open('GET', '/decrease' + color, true);"
+         "xhr.send();}</script>\n";
+  ptr += "<style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
+  ptr += "body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;}\n";
+  ptr += "p {font-size: 24px;color: #444444;margin-bottom: 10px;}\n";
+  ptr += "</style>\n";
+  ptr += "</head>\n";
+  ptr += "<body>\n";
+  ptr += "<div id=\"webpage\">\n";
+  ptr += "<h1>KASZTAN Weather Station</h1>\n";
+  ptr += "<p>Temperature: ";
+  ptr += temperature;
+  ptr += "&deg;C</p>";
+  ptr += "<p>Humidity: ";
+  ptr += humidity;
+  ptr += "%</p>";
+  ptr += "<p>Light: ";
+  ptr += light;
+  ptr += "</p>";
+  ptr += "<p>RGB LED:</p>";
+  ptr += "<button onclick=\"increaseColor('Red')\">Increase Red</button>";
+  ptr += "<button onclick=\"decreaseColor('Red')\">Decrease Red</button><br>";
+  ptr += "<button onclick=\"increaseColor('Green')\">Increase Green</button>";
+  ptr += "<button onclick=\"decreaseColor('Green')\">Decrease Green</button><br>";
+  ptr += "<button onclick=\"increaseColor('Blue')\">Increase Blue</button>";
+  ptr += "<button onclick=\"decreaseColor('Blue')\">Decrease Blue</button><br>";
+  ptr += "<p>by Robert Zubek</p>";
   ptr +="</div>\n";
   ptr +="</body>\n";
   ptr +="</html>\n";
